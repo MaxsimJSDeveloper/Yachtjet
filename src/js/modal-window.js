da588f233300a2406d8b3ada51b3221e1655526c
MaxsimJSDeveloper <147420858+MaxsimJSDeveloper@users.noreply.github.com>
2024-03-08 11:20:33 +0200

    form-js

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,22 +1,20 @@
+const cssClasses = {
+  burgerButton: 'burger-menu',
+  modal: 'modal-window',
+  closeButton: 'modal-menu-close-btn',
+  toggleClass: 'is-open',
+  menuButtons: 'modal-menu-item',
+};
 
-      const cssClasses = {
-        burgerButton: 'burger-menu',
-        modal: 'modal-window',
-        closeButton: 'modal-menu-close-btn',
-        toggleClass: 'is-open',
-        menuButtons: 'modal-menu-item',
-      };
+const burgerMenu = document.querySelector(`.${cssClasses.burgerButton}`);
+const modal = document.querySelector(`.${cssClasses.modal}`);
+const closeButton = document.querySelector(`.${cssClasses.closeButton}`);
+const menuButtons = document.querySelectorAll('.modal-menu-item');
 
-      const burgerMenu = document.querySelector(`.${cssClasses.burgerButton}`);
-      const modal = document.querySelector(`.${cssClasses.modal}`);
-      const closeButton = document.querySelector(`.${cssClasses.closeButton}`);
-      const menuButtons = document.querySelectorAll(".modal-menu-item")
+function toggle(event) {
+  modal.classList.toggle(cssClasses.toggleClass);
+}
 
-      function toggle (event) {       
-        modal.classList.toggle(cssClasses.toggleClass);
-      };
-
-      burgerMenu.addEventListener('click', toggle);
-      closeButton.addEventListener('click', toggle);
-      menuButtons.forEach(button => button.addEventListener('click', toggle));
-    
+burgerMenu.addEventListener('click', toggle);
+closeButton.addEventListener('click', toggle);
+menuButtons.forEach(button => button.addEventListener('click', toggle));
